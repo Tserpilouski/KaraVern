2ff0f1635a1aa65ea90b8f37e04e03e2b253e9f0
Kiryl Tserpilouski <terpilovskikirill@gmail.com>
2023-07-02 12:35:48 +0200

    end working on about screen

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,6 +1,17 @@
 import NavBar from '../componets/NavBar';
-import Footer from '../componets/Footer';
 import Welcome from '../componets/welcome';
+import StudingHome from '../componets/StudingHome';
+import Footer from '../componets/Footer';
+
+import insta1 from '../assets/insta/insta1.png';
+import insta2 from '../assets/insta/insta2.png';
+import insta3 from '../assets/insta/insta3.png';
+import insta4 from '../assets/insta/insta4.png';
+import insta5 from '../assets/insta/insta5.png';
+import insta6 from '../assets/insta/insta6.png';
+import insta7 from '../assets/insta/insta7.png';
+import insta8 from '../assets/insta/insta8.png';
+import insta9 from '../assets/insta/insta9.png';
 
 import '../styles/views/aboutView.scss';
 
@@ -10,16 +21,27 @@ const About = () => {
       <NavBar />
       <Welcome />
       <div className='about'>
-        <h2>Changing the digital world</h2>
+        <h2>Każda z nas zasługuje na to by czuć się pięknie!</h2>
         <p>
-          I believe it's paramount to create a committed and genuine community
-          that has a positive influence on people’s lives beyond the online
-          sphere. With more people online than ever before, the competition for
-          attention is fierce, and you have to work hard to appeal to the
-          masses. That's why it's my goal to help you understand the power of
-          social media and create connections that go beyond the internet.
+          Witam serdecznie na mojej stronie. Nazywam się Kara Vern. Makijażem
+          permanentnym zajmuję się od 2018r. Zawód linergisty stał się nie tylko
+          moją pracą, ale także moją pasją. Moim celem jest sprawiać aby każda
+          klientka po zabiegu czuła się piękniejsza i pewniejsza siebie.
         </p>
+        <h2>Zapraszam do obserwowania na Instagram</h2>
+        <div className='about__fotobox'>
+          <img src={insta1} alt='' />
+          <img src={insta2} alt='' />
+          <img src={insta3} alt='' />
+          <img src={insta4} alt='' />
+          <img src={insta5} alt='' />
+          <img src={insta6} alt='' />
+          <img src={insta7} alt='' />
+          <img src={insta8} alt='' />
+          <img src={insta9} alt='' />
+        </div>
       </div>
+      <StudingHome />
       <Footer />
     </div>
   );
